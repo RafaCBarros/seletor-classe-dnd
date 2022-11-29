58f2f855300cb57a8000b5b6b14bac124188540a
Rafaela Barros <82919168+RafaelaBarros@users.noreply.github.com>
2022-11-29 11:39:06 -0300

    Atualizar Verificador
    Atualização da lógica que verifica a classe baseado nos atributos.

--- src/utils/verificadores.ts
+++ src/utils/verificadores.ts
@@ -1,76 +1,66 @@
 import {TipoFichaPersonagem as FichaPersonagem} from '../data/TiposLocais'
 
 // Classifica valor entre Baixo-Alto
-function classificarValor(atributo: number): 0 | 1 | 2 {
+/* function classificarValor(atributo: number): 0 | 1 | 2 {
   if(atributo < 10) return 0; //Baixo
   else if (atributo >= 16) return 2; //Alto
   else return 1; //Médio-Alto
 }
+*/
+function aventureiroViavel(personagem: FichaPersonagem) {
+  const totalAtributo = personagem.carisma 
+  +personagem.constituicao 
+  +personagem.destreza 
+  +personagem.inteligencia 
+  +personagem.forca 
+  +personagem.sabedoria
 
+  if (totalAtributo > 65) return true
+  else return false;
+}
 // Verifica classe com base no valor
 function verificarClasse(personagem: FichaPersonagem):
-"Feitceiro" | "Bruxo" | "Paladino" | "Bardo" | "Barbaro" | "Clerigo" | "Guerreiro" | "Monge" | "Guardiao" | "Ladino" | "Druida" | "Mago" | "Fazendeiro" | undefined{
-  if(classificarValor(personagem.carisma) === 2) {
-    if(classificarValor(personagem.constituicao) === 1)
-      return "Feitceiro";
-    else if(classificarValor(personagem.constituicao) === 2)
-      return "Bruxo";
-  } else if(classificarValor(personagem.carisma) === 1) {
-    if(classificarValor(personagem.constituicao) >= 1)
-      if(classificarValor(personagem.forca) === 2)
-        return "Paladino";
-  } else if(classificarValor(personagem.destreza) === 1) {
-    if(classificarValor(personagem.carisma) === 2)
-      return "Bardo";
-    else if(classificarValor(personagem.forca) >= 1) {
-      if(classificarValor(personagem.constituicao) === 2)
-        return "Barbaro";
-      else if (classificarValor(personagem.constituicao) === 1) {
-        if (classificarValor(personagem.sabedoria) === 2)
-          return "Clerigo";
-        else if (classificarValor(personagem.sabedoria) === 0)
-          return "Guerreiro";
+0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11 | 12 {
+  
+  if(aventureiroViavel(personagem)){
+    if (personagem.inteligencia >= 16)
+      return 11; // Mago
+  
+    if (personagem.sabedoria >= 16){
+      if (personagem.constituicao >= 14)
+        return 5; // Clerigo
+      return 10; // Druída
+    }
+
+    if(personagem.carisma >= 16) {
+      if(personagem.constituicao >= 14){
+        return 1; // Bruxo
       }
+      else if(personagem.constituicao > 10)
+        return 0; // Feiticeiro
+      return 3; // Bardo
     }
-  } else if (classificarValor(personagem.destreza) === 2) {
-    if (classificarValor(personagem.sabedoria) >= 1) {
-      if(classificarValor(personagem.constituicao) >= 1)
-        return "Monge";
-      else return "Guardiao";
+    
+    if(personagem.forca >= 14) { // Guerreiro, Barbaro, Paladino
+      if(personagem.constituicao >= 16)
+        return 4; // Barbaro
+      if(personagem.carisma >= 14) 
+        return 2; // Paladino
+      if(personagem.destreza >= 10 || personagem.constituicao >= 10)
+        return 6; // Guerreiro
     }
-    else if (classificarValor(personagem.carisma) === 1)
-      return "Ladino";
-  } else if (classificarValor(personagem.sabedoria) === 2)
-    return "Druida";
-  else if (classificarValor(personagem.inteligencia) === 2)
-    return "Mago";
-  else return "Fazendeiro";
+
+    if(personagem.destreza >= 14) { //Guardião, Monge, Ladino
+      if (personagem.sabedoria >= 14) {
+        if (personagem.constituicao >= 12)
+          return 7; // Monge
+        return 8; // Guardião
+      }
+      return 9; // Ladino
+    }
+  }
+  
+  return 12; // Fazendeiro
 }
 
 export { verificarClasse }
-
-
-/*
-carisma == 2 // Bruxo, Feiticeiro, Bardo
-    constituicao == 1: Feitceiro
-    constituicao == 2: Bruxo
-  carisma == 1 // Paladino, Ladino
-    constituicao >= 1 // Paladino
-      força == 2: Paladino
-  destreza == 1 // Barbaro, Bardo, Guerreiro
-    carisma == 2: Bardo
-    força >= 1: Barbaro, Guerreiro, Clerigo
-      constituicao == 2: Barbaro
-      constituicao == 1 // Guerreiro, Clerigo
-        sabedoria == 2: Clerigo
-        sabedoria <= 1: Guerreiro
-  destreza == 2 // Ladino, Guardiao, Monge
-    sabedoria >= 1 // Guardiao, Monge
-      constituicao >= 1: Monge
-      constituicao < 1: Guardiao
-    carisma == 1: Ladino
-  sabedoria == 2: Druida
-  inteligencia == 2: Mago
-  Fazendeiro
-
-*/
